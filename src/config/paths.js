033d48704a7be9f4bf18a8bcc00381646f7010b3
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-10 13:17:36 -0300

    Added chakra icons dependency and default config paths

---
+++ src/config/paths.js
@@ -0,0 +1,17 @@
+export const paths = {
+    lading: "/",
+    login: "/login",
+    register: "/registro",
+    account: "/micuenta",
+    aboutUs: "/acerca",
+    contacts: "/contactos",
+    why: "/porque",
+    registerHouse: "/inmuebles/registrar",
+    registerPost: "/publicaciones/registrar",
+    search: "/buscar",
+    postList: "/publicaciones",
+    postDetail: "/publicaciones/detalle/:id",
+    forgetterpass: "/recuperar",
+    members: "/integrantes",
+  };
+  
