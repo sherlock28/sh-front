69b64dd29e008cfff4f54ff08c9a34be18969525
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-11 20:06:29 -0300

    Added Navbar component

---
+++ src/components/commons/Navbar/MobileNav.js
@@ -0,0 +1,19 @@
+import {
+    Stack,
+    useColorModeValue,
+} from '@chakra-ui/react';
+import { MobileNavItem } from './MobileNavItem';
+import { NAV_ITEMS } from 'config/navbarItems';
+
+export function MobileNav() {
+    return (
+        <Stack
+            bg={useColorModeValue('white', 'gray.800')}
+            p={4}
+            display={{ md: 'none' }}>
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    );
+};
