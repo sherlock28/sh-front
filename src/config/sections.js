ad39b0ddbb921085ad72a59d87ddaf2c81437e71
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-12 20:48:27 -0300

    Added sections and validations config. Installed react-google-recaptcha

---
+++ src/config/sections.js
@@ -0,0 +1,47 @@
+export const sections = {
+    aboutus: {
+        section: "AboutUs",
+        title: "",
+    },
+    forgotterpass: {
+        section: "ForgotterPass",
+        title: "",
+    },
+    home: {
+        section: "Home",
+        title: "",
+    },
+    main: {
+        section: "Main",
+        title: "",
+    },
+    postdetail: {
+        section: "PostDetail",
+        title: "",
+    },
+    profile: {
+        section: "Profile",
+        titleAccount: "Mi cuenta",
+        titlePublications: "Mis publicaciones",
+    },
+    search: {
+        section: "Search",
+        title: "Buscar",
+    },
+    login: {
+        section: "Login",
+        title: "Accedé a tu cuenta",
+    },
+    register: {
+        section: "Register",
+        title: "Registrarme como ",
+    },
+    registerHouse: {
+        section: "RegisterHouse",
+        title: "Registra tu inmueble",
+    },
+    registerPublication: {
+        section: "RegisterPublication",
+        title: "Publica tu inmueble",
+    }
+};
