f28b67d1a83dfa70d425bd05f25a640784063208
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-13 20:58:14 -0300

    Added store initial config

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { users } from "services";
+
+const initialState = {
+    user: null,
+    currentUser: null,
+    isFetching: false,
+    isSuccess: false,
+    isError: false,
+    errorMessage: "",
+};
+
+export const userSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        userLoading: (state) => {
+            if (state.isFetching === false) {
+                state.isFetching = true;
+            }
+        },
+        successGetCurrentUser: (state, action) => {
+            state.isFetching = false;
+            state.currentUser = action.payload;
+            state.isSuccess = true;
+        },
+        successUserUpdate: (state, action) => {
+            state.isFetching = false;
+            state.isSuccess = true;
+        },
+        failed: (state, action) => {
+            state.isFetching = false;
+            state.isError = true;
+            state.errorMessage = action.payload.error;
+        },
+        clearState: (state, action) => {
+            state.isSuccess = false;
+            state.isError = false;
+            state.errorMessage = "";
+        },
+    },
+});
+
+export const {
+    userLoading,
+    successGetCurrentUser,
+    successUserUpdate,
+    failed,
+    clearState,
+} = userSlice.actions;
+
+export const getCurrentUserAction = () => async (dispatch) => {
+    dispatch(userLoading());
+
+};
+
+export const updateUserAction = (data) => async (dispatch) => {
+    dispatch(userLoading());
+
+};
+
+export default userSlice.reducer;
+
+export const userSelector = (state) => state.user;
+export const currentUserSelector = (state) => state.user.currentUser;
