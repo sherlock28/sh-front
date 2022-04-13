7e2c5b43fa3fb29afe8d29cf204725cb6e51ad6c
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-04-12 21:16:42 -0300

    Added SectionHeader component

---
+++ src/components/commons/SectionHeader/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Box, Heading, Text, Link as LinkChakra } from "@chakra-ui/react";
+import { Link } from "wouter";
+import { paths } from "config/paths";
+
+export function SectionHeader({ section, sectionTitle, isOwner }) {
+  let title = null;
+  let subTitle = null;
+
+  if (section === "Login") {
+    title = (<Heading>{sectionTitle}</Heading>);
+    subTitle = (
+      <Text mt={4}>
+        ¿Todavía no tenés cuenta en Segundo Hogar?{" "}
+        <Link to={paths.register}>
+          <LinkChakra color={'blue.400'}>Clic aquí</LinkChakra>
+        </Link>
+      </Text>
+    );
+  }
+
+  if (section === "Register") {
+    title = (
+      <Heading>
+        {sectionTitle}{isOwner ? "propietario" : "estudiante"}
+      </Heading>
+    );
+    subTitle = (
+      <>
+        <Text mt={4}>
+          ¿Ya tenés cuenta con Segundo Hogar?{" "}
+          <Link to={paths.login}>
+            <LinkChakra color={'blue.400'}>Clic aquí</LinkChakra>
+          </Link>
+        </Text>
+      </>
+    );
+  }
+
+  if (section === "Profile") {
+    title = (<Heading>{sectionTitle}</Heading>);
+  }
+
+  if (section === "Search") {
+    title = (<Heading>{sectionTitle}</Heading>);
+  }
+
+  if (section === "RegisterHouse") {
+    title = (<Heading>{sectionTitle}</Heading>);
+  }
+
+  if (section === "RegisterPublication") {
+    title = (<Heading>{sectionTitle}</Heading>);
+  }
+
+  return (
+    <Box textAlign="center">
+      {title}
+      {subTitle}
+    </Box>
+  );
+}
