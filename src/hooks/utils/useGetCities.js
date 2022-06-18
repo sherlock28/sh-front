b206f1cb6cd7d085af655f295e09176110c435ea
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-18 08:20:56 -0300

    Added: Hook for get cities using graphql

---
+++ src/hooks/utils/useGetCities.js
@@ -0,0 +1,8 @@
+import { useQuery } from '@apollo/client';
+import { GET_CITIES } from 'client/gql/utils';
+
+export function useGetCities() {
+    const { loading, error, data: cities } = useQuery(GET_CITIES);
+
+    return { loading, error, cities };
+}
