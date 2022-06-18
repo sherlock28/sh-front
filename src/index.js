9beef5e2a144f8b8d2e1f8bf2a1dda9e64f5a9ed
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-18 08:19:30 -0300

    Using ApolloProvider as context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { client } from 'client/client';
+import { ApolloProvider } from '@apollo/client';
 import './index.css';
 import { App } from './App';
 import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <ApolloProvider client={client}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </ApolloProvider>
 );
