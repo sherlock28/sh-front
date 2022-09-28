2dc129670c8bed39774cd7bc407d6097aea224d2
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-09-28 00:39:02 -0300

    Added: GraphQl client

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,17 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloLink, createHttpLink } from "@apollo/client";
+
+const httpLink = new createHttpLink({ uri: process.env.REACT_APP_API_GRAPHQL });
+
+const authLink = new ApolloLink((operation, forward) => {
+    operation.setContext({
+        headers: {
+            "x-hasura-admin-secret": process.env.REACT_APP_GRAPHQL_KEY
+        }
+    });
+    return forward(operation);
+});
 
 export const client = new ApolloClient({
-    uri: process.env.REACT_APP_API_GRAPHQL,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
