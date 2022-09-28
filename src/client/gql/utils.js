671f949c3aa4375d62631256c1eece31adbbf1ed
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-09-28 00:44:53 -0300

    Adding queries for fetch: states, cities, careers

--- src/client/gql/utils.js
+++ src/client/gql/utils.js
@@ -1,12 +1,28 @@
 import { gql } from "@apollo/client";
 
+export const GET_STATES = gql`
+    query GetStates {
+        sh_states {
+            id
+            name
+        }
+    }  
+`;
+
 export const GET_CITIES = gql`
     query GetCities {
         sh_cities {
             id
             name
         }
-    }
+    }  
 `;
 
-  
+export const GET_CAREERS = gql`
+    query GetCarrers {
+        sh_carrers {
+            id
+            name
+        }
+    }  
+`;
