ea1142bdf5c33c234a8cea4a4597f217c3dd5cdb
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-10-13 04:56:24 -0300

    Adding the 'useGetPublicationById' hook

---
+++ src/hooks/pages/PublicationDetail/useGetPublicationById.js
@@ -0,0 +1,8 @@
+import { useQuery } from '@apollo/client';
+import { SEARCH_FOR_DETAILS } from 'client/gql/searches/searches';
+
+export function useGetPublicationById({ id }) {
+    const { loading, error, data: publication } = useQuery(SEARCH_FOR_DETAILS, { variables: { id: id } });
+
+    return { loading, error, publication: publication?.sh_publications[0] };
+}
