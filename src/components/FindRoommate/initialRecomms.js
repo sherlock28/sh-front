68e4914d46869a726ac0ffd5bda59770cf9e45c9
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-10-25 08:39:33 -0300

    Added test data for recommendations

---
+++ src/components/FindRoommate/initialRecomms.js
@@ -0,0 +1,62 @@
+export const initialRecomms = [
+    {
+        id: 1,
+        avatar: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        username: 'johndoe',
+        name: 'John Doe',
+        career: 'Ing en Sistemas de Información',
+        age: 20,
+        bio: 'Frontend Developer',
+        tags: ["musica", "frontend", "tecnologia"]
+    },
+    {
+        id: 2,
+        avatar: 'https://bit.ly/ryan-florence',
+        username: 'ryanf',
+        name: 'Ryan Florence',
+        career: 'Ing en Sistemas de Información',
+        age: 25,
+        bio: 'Actress, musician, songwriter and artist. PM for work inquires or #tag me in your posts',
+        tags: ["cine", "futbol", "backend"]
+    },
+    {
+        id: 3,
+        avatar: 'https://bit.ly/sage-adebayo',
+        username: 'sageadebayo',
+        name: 'Sage Adebayo',
+        career: 'Ing en Sistemas de Información',
+        age: 25,
+        bio: 'Actress, musician, songwriter and artist. PM for work inquires or #tag me in your posts',
+        tags: ["futbol", "fullstack", "videojuegos"]
+    },
+    {
+        id: 4,
+        avatar: 'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        username: 'lindsey_jam3s',
+        name: 'Lindsey James',
+        career: 'Ing en Sistemas de Información',
+        age: 25,
+        bio: 'Actress, musician, songwriter and artist. PM for work inquires or #tag me in your posts',
+        tags: ["tecnologia", "diseño", "arte"]
+    },
+    {
+        id: 5,
+        avatar: 'https://bit.ly/kent-c-dodds',
+        username: 'kdodds',
+        name: 'Kent C. Dodds',
+        career: 'Ing en Sistemas de Información',
+        age: 25,
+        bio: 'Actress, musician, songwriter and artist. PM for work inquires or #tag me in your posts',
+        tags: ["videojuegos", "series", "tecnologia"]
+    },
+    {
+        id: 6,
+        avatar: 'https://bit.ly/prosper-baba',
+        username: 'prosperbaba',
+        name: 'Prosper Baba',
+        career: 'Ing en Sistemas de Información',
+        age: 25,
+        bio: 'Actress, musician, songwriter and artist. PM for work inquires or #tag me in your posts',
+        tags: ["libros", "tecnologia", "musica"]
+    }
+];
