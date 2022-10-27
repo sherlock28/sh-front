caf346adda85d024987fd8f1e8d9ef13d42014b8
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-10-27 08:26:52 -0300

    Added custom hook for calling APIs using axios

---
+++ src/hooks/utils/useAxios.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL_RECOMM;
+
+export const useAxios = (axiosParams) => {
+    const [response, setResponse] = useState(undefined);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async (params) => {
+        try {
+            const result = await axios.request(params);
+            setResponse(result.data);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(axiosParams);
+    }, []);
+
+    return { response, error, loading };
+};
