4770d64670cca790b327e71b51ee0f674fdc99ca
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-11-15 00:49:30 -0300

    Added more params to  CustomButton component

--- src/components/commons/CustomButton/index.js
+++ src/components/commons/CustomButton/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-export function CustomButton({ width, textButton, isLoading, handleClick, margin }) {
+export function CustomButton({ width, textButton, isLoading, handleClick, margin, background, color, hoverBg }) {
+
+    let hoverBackground = hoverBg ? hoverBg : "#36393f";
+
     return (
         <Button
             onClick={handleClick}
             isLoading={isLoading}
             rounded="md"
-            bg="black"
-            color="white"
+            bg={background ? background : "black"}
+            color={color ? color : "white"}
             width={width}
             margin={margin}
-            _hover={{ background: "#36393f", transform: 'translateY(-2px)',  boxShadow: 'lg' }}
+            _hover={{ background: hoverBackground, transform: 'translateY(-2px)',  boxShadow: 'lg' }}
             _focus={{ outline: "none", border: "none " }}
         >
             {textButton}
