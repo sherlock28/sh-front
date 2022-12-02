ad35ad999f1a9c11da56ceaa46741b63f50ea782
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-12-01 23:42:12 -0300

    Changes in hook: useAxios

--- src/hooks/utils/useAxios.js
+++ src/hooks/utils/useAxios.js
@@ -1,27 +1,36 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL_RECOMM;
-
-export const useAxios = (axiosParams) => {
+export const useAxios = ({ url, method, body = null, headers = null }) => {
     const [response, setResponse] = useState(undefined);
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(true);
 
-    const fetchData = async (params) => {
-        try {
-            const result = await axios.request(params);
-            setResponse(result.data);
-        } catch (error) {
-            setError(error);
-        } finally {
-            setLoading(false);
+    const fetchData = () => {
+        setLoading(true);
+
+        let config = {
+            method: method,
+            url: url,
+            headers: JSON.parse(headers),
+            data: JSON.parse(body)
         }
+
+        axios(config)
+            .then((res) => {
+                setResponse(res?.data);;
+            })
+            .catch((err) => {
+                setError(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     };
 
     useEffect(() => {
-        fetchData(axiosParams);
-    }, []);
+        fetchData();
+    }, [method, url, body, headers]);
 
     return { response, error, loading };
 };
