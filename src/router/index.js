d8e2bbe22ac67beadbd80662fd58ea2e06bacf38
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-01-16 23:33:32 -0300

    Using ProtectedRoute component in Routes

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,7 @@ import { HouseRegisterView } from "views/HouseRegister";
 import { PublicationRegisterView } from "views/PublicationRegister";
 import { FindRoommateView } from "views/FindRoommate";
 import { RoommateProfileView } from "views/RoommateProfile";
+import { ProtectedRoute } from "components/commons/ProtectedRoute";
 import { paths } from "config/paths";
 
 export function Router() {
@@ -23,18 +24,22 @@ export function Router() {
       <Route path={paths.landing} component={LandingView} exact />
       <Route path={paths.login} component={SignInView} exact />
       <Route path={paths.register} component={SignUpView} exact />
-      <Route path={paths.tags} component={TagsView} exact />
-      <Route path={paths.questions} component={QuestionsView} exact />
-      <Route path={paths.account} component={ProfileView} exact />
       <Route path={paths.forgetterpass} component={ForgotterPassView} exact />
       <Route path={paths.search} component={SearchView} exact />
-      <Route path={paths.publicationDetail} component={PublicationDetailView} exact />
-      <Route path={paths.houseRegister} component={HouseRegisterView} exact />
-      <Route path={paths.publicationRegister} component={PublicationRegisterView} exact />
       <Route path={paths.members} component={MembersView} exact />
       <Route path={paths.aboutUs} component={AboutUsView} exact />
-      <Route path={paths.findRoommate} component={FindRoommateView} exact />
-      <Route path={paths.roommateAccount} component={RoommateProfileView} exact />
+      <Route path="/" component={
+        <ProtectedRoute>
+          <Route path={paths.tags} component={TagsView} exact />
+          <Route path={paths.questions} component={QuestionsView} exact />
+          <Route path={paths.account} component={ProfileView} exact />
+          <Route path={paths.publicationDetail} component={PublicationDetailView} exact />
+          <Route path={paths.houseRegister} component={HouseRegisterView} exact />
+          <Route path={paths.publicationRegister} component={PublicationRegisterView} exact />
+          <Route path={paths.findRoommate} component={FindRoommateView} exact />
+          <Route path={paths.roommateAccount} component={RoommateProfileView} exact />
+        </ProtectedRoute>
+      } />
       <Route>404, Not Found!</Route>
     </Switch>
   )
