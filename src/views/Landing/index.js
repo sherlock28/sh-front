9d625227b28ca026967b602824ede234dd4bbb12
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-01-24 09:02:39 -0300

    Redirecting landing page to '/search' when user is authenticated

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Landing } from 'components/Landing'
+import { useLocation, Redirect } from "wouter";
+import { Landing } from 'components/Landing';
+import { useSelector } from "react-redux";
+import { authSelector } from "store/slices/authSlice";
 
 export function LandingView() {
-    return (
-        <Landing/>
-    )
+
+    const [location] = useLocation();
+    const { isAuthenticated } = useSelector(authSelector);
+
+    if (isAuthenticated) return <Redirect to="/buscar" state={{ from: location }} replace />
+
+    return <Landing />
 }
