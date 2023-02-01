368c0525d114ff7f6ab2c411b15b2f134ee4deae
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-02-01 08:47:43 -0300

    Added: construction of the query for the search

--- src/client/gql/queries/searches/searches.js
+++ src/client/gql/queries/searches/searches.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { ANY_OWNERSHIPS_TYPE, HOUSE_OWNERSHIPS_TYPE, DEPARTMENT_OWNERSHIPS_TYPE } from "const";
 
 export const GET_INITIAL_PUBLICATIONS = gql`
     query SearchForCards($limit: Int, $offset: Int) {
@@ -21,29 +22,6 @@ export const GET_INITIAL_PUBLICATIONS = gql`
             }
         }
     }  
-`;
-
-export const SEARCH_PUBLICATIONS = gql`
-query SearchForCards($limit: Int, $offset: Int) {
-    sh_publications(limit: $limit, offset: $offset) {
-        id
-        title
-        price
-        ownership {
-            id
-            rooms
-            bathrooms
-            rating
-            ownerships_images {
-                imageurl
-            }
-            coordinate {
-                lat
-                lon
-            }
-        }
-    }
-}  
 `;
 
 export const SEARCH_FOR_DETAILS = gql`
@@ -73,3 +51,57 @@ query SearchForDetails($id: Int) {
     }
 }
 `;
+
+export function buildSearchQueryUsingFilters(filters) {
+
+  const page_opt = `
+    limit: ${filters.limit}, 
+    offset: ${filters.offset}
+  `;  
+
+  const ownershipsType = filters.ownerships_type === "Departamento" ? DEPARTMENT_OWNERSHIPS_TYPE : HOUSE_OWNERSHIPS_TYPE;
+
+  const ownerships_type = filters.ownerships_type === ANY_OWNERSHIPS_TYPE ? "" : `{ ownership: { ownerships_type: { id: { _eq: ${ownershipsType} } } } },`;
+
+  const where_and = `
+    { price: { _gte: ${filters.pricemin}, _lte: ${filters.pricemax} } }
+    ${filters.ownerships_type === ANY_OWNERSHIPS_TYPE ? "" : ownerships_type} 
+    { is_furnished: { _eq: ${filters.is_furnished} } },
+    { ownership: { rooms: { _lte: ${filters.rooms} } } },
+    { ownership: { bathrooms: { _lte: ${filters.bathrooms} } } },
+    { ownership: { size: { _lte: ${filters.size} } } }
+  `;
+
+  console.log(where_and)
+
+  const query = `
+  query SearchUsingFilters {
+    sh_publications(
+      ${page_opt},
+      where: { 
+        _and: [ ${where_and} ]
+      }
+    ) 
+      {
+        id
+        title
+        price
+        ownership {
+          id
+          rooms
+          bathrooms
+          rating
+          ownerships_images {
+              imageurl
+            }
+          coordinate {
+              lat
+              lon
+          }
+        }
+      }
+    }
+  `;
+
+  return query;
+}
