88857884b9297a0be5e89ed4d8180486acb5e287
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-02-16 00:52:03 -0300

    Showing TabPanel for each type of user

--- src/components/Profile/AccountTabs.js
+++ src/components/Profile/AccountTabs.js
@@ -3,11 +3,13 @@ import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { SectionHeader } from "components/commons/SectionHeader";
 import { ProfileForm } from "./ProfileForm";
+import { ProfileFormOwner } from "./ProfileFormOwner";
 import { PublicationsList } from "./PublicationsList";
 import { Tags } from "components/Tags";
 import { Questions } from 'components/Questions';
 import { sections } from "config/sections";
 import { authSelector } from "store/slices/authSlice";
+import { USER_CATEGORIES } from "const";
 
 const PAGE = "profile";
 
@@ -16,16 +18,27 @@ export function AccountTabs() {
   const { profile, tags, questions } = sections;
 
   const { user_category } = useSelector(authSelector);
-  console.log({user_category})
 
   return (
     <>
       <Tabs variant="soft-rounded" colorScheme="gray">
         <TabList>
           <Tab>Mi cuenta</Tab>
-          <Tab>Publicaciones</Tab>
-          <Tab>Intereses</Tab>
-          <Tab>Preguntas</Tab>
+          {
+            user_category === USER_CATEGORIES.OWNER ?
+              <Tab>Publicaciones</Tab>
+              : <></>
+          }
+          {
+            user_category === USER_CATEGORIES.STUDENT ?
+              <Tab>Intereses</Tab>
+              : <></>
+          }
+          {
+            user_category === USER_CATEGORIES.STUDENT ?
+              <Tab>Preguntas</Tab>
+              : <></>
+          }
         </TabList>
         <TabPanels>
           <TabPanel>
@@ -33,31 +46,46 @@ export function AccountTabs() {
               section={profile.section}
               sectionTitle={profile.titleAccount}
             />
-            <ProfileForm />
+            {
+              user_category === USER_CATEGORIES.STUDENT ?
+                <ProfileForm />
+                : <ProfileFormOwner />
+            }
           </TabPanel>
 
-          <TabPanel>
-            <SectionHeader
-              section={profile.section}
-              sectionTitle={profile.titlePublications}
-            />
-            <PublicationsList />
-          </TabPanel>
-
-          <TabPanel>
-            <SectionHeader
-              section={tags.section}
-              sectionTitle={tags.title}
-            />
-            <Tags fromPage={PAGE} />
-          </TabPanel>
-          <TabPanel>
-            <SectionHeader
-              section={questions.section}
-              sectionTitle={questions.title}
-            />
-            <Questions fromPage={PAGE} />
-          </TabPanel>
+          {
+            user_category === USER_CATEGORIES.OWNER ?
+              <TabPanel>
+                <SectionHeader
+                  section={profile.section}
+                  sectionTitle={profile.titlePublications}
+                />
+                <PublicationsList />
+              </TabPanel>
+              : <></>
+          }
+          {
+            user_category === USER_CATEGORIES.STUDENT ?
+              <TabPanel>
+                <SectionHeader
+                  section={tags.section}
+                  sectionTitle={tags.title}
+                />
+                <Tags fromPage={PAGE} />
+              </TabPanel>
+              : <></>
+          }
+          {
+            user_category === USER_CATEGORIES.STUDENT ?
+              <TabPanel>
+                <SectionHeader
+                  section={questions.section}
+                  sectionTitle={questions.title}
+                />
+                <Questions fromPage={PAGE} />
+              </TabPanel>
+              : <></>
+          }
         </TabPanels>
       </Tabs>
     </>
