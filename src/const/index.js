8e878534b2aeaa93a9e9a1087f83072600f862a2
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-02-16 00:50:37 -0300

    Adding new constant 'USER_CATEGORIES'

--- src/const/index.js
+++ src/const/index.js
@@ -1,3 +1,9 @@
 export const ANY_OWNERSHIPS_TYPE = -1;
 export const DEPARTMENT_OWNERSHIPS_TYPE = 1;
 export const HOUSE_OWNERSHIPS_TYPE = 2;
+
+export const USER_CATEGORIES = {
+    ADMIN: 1,
+    STUDENT: 2,
+    OWNER: 3
+}
