4ac935a90113437db8e88bc5ad5840c0a27fe7e5
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-03-01 23:33:38 -0300

    Updated: 'selectedTagsFiltered'

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -15,6 +15,12 @@ import { sections } from "config/sections";
 const REGISTAR = "Registrar";
 const GUARDAR = "Guardar";
 
+const sportsIds = [1, 2, 3, 4, 5, 6, 7];
+const petsIds = [8, 9, 10, 11, 12, 13, 14, 15];
+const genresIds = [16, 17, 18, 19, 21, 22, 23];
+const musicGenresIds = [24, 25, 26, 27, 28, 29, 30];
+const hobbiesids = [31, 32, 33, 34, 35, 36, 37];
+
 export function Tags({ fromPage }) {
     const [selectedTags, setSelectedTags] = useState([]);
 
@@ -35,8 +41,41 @@ export function Tags({ fromPage }) {
         const questionArr = JSON.parse(window.localStorage.getItem("questionsValue"));
         const selectedTagsFiltered = [...new Set(selectedTags)];
 
-        console.log(selectedTagsFiltered)
-        console.log(questionArr)
+        const sports = [];
+        const pets = [];
+        const genremusic = [];
+        const genremovies = [];
+        const hobbies = [];
+
+        selectedTagsFiltered.forEach(tag => {
+            if (sportsIds.includes(tag)) sports.push(tag)
+            if (petsIds.includes(tag)) pets.push(tag)
+            if (genresIds.includes(tag)) genremusic.push(tag)
+            if (musicGenresIds.includes(tag)) genremovies.push(tag)
+            if (hobbiesids.includes(tag)) hobbies.push(tag)
+        });
+
+        const body = {
+            "preferences": {
+                sports,
+                genremovies,
+                genremusic,
+                hobbies,
+                pets,
+            },
+            "lifestyles": questionArr
+        }
+
+        console.log(body)
+
+        fetch(process.env.REACT_APP_API_URL_RECOMM, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => response.json())
+            .then(res => console.log(res))
+            .catch(err => err);
     }
 
 
@@ -65,12 +104,12 @@ export function Tags({ fromPage }) {
                                 color='black'
                                 fontWeight='medium'
                                 textAlign='center'
-                                background={isSelectedElem(tg?.description) ? '#F5F5F5' : 'white'}
-                                cursor={isSelectedElem(tg?.description) ? 'pointer' : ''}
-                                transform={isSelectedElem(tg?.description) ? 'translateY(-2px)' : ''}
-                                boxShadow={isSelectedElem(tg?.description) ? 'lg' : 'md'}
+                                background={isSelectedElem(tg?.id) ? '#F5F5F5' : 'white'}
+                                cursor={isSelectedElem(tg?.id) ? 'pointer' : ''}
+                                transform={isSelectedElem(tg?.id) ? 'translateY(-2px)' : ''}
+                                boxShadow={isSelectedElem(tg?.id) ? 'lg' : 'md'}
                                 _hover={{ cursor: 'pointer', background: "#F5F5F5", transform: 'translateY(-2px)', boxShadow: 'lg' }}
-                                onClick={() => selectTag(tg?.description)}
+                                onClick={() => selectTag(tg?.id)}
                             >
                                 {tg?.description}
                             </Box>
