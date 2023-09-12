b336ddb29d9dd0117991d9cdd799e650abf3b024
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-09-11 23:50:29 -0300

    Add REGISTER_STUDENT_USER_USER_WITH_SOC_NET mutation

--- src/client/gql/mutations/registerUser/registerStudentUserSocialNetwork.js
+++ src/client/gql/mutations/registerUser/registerStudentUserSocialNetwork.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
-export const REGISTER_STUDENT_USER_WITH_SOC_NET = gql`
-mutation RegisterStudentUserSocialNetworks($lastname: String, $firstname: String, 
+export const INITIAL_REGISTER_STUDENT_USER_WITH_SOC_NET = gql`
+mutation InitialRegisterStudentUserSocialNetworks($lastname: String, $firstname: String, 
   $email: String, $created_with_sn: Boolean, $user_status: Boolean)
     {
       insert_sh_persons_one(object: 
@@ -27,5 +27,74 @@ mutation RegisterStudentUserSocialNetworks($lastname: String, $firstname: String
           created_with_sn
         }
       }
+    }
+`;
+
+
+export const REGISTER_STUDENT_USER_USER_WITH_SOC_NET = gql`
+mutation RegisterStudentUser($gender: String, $birth_date: date, $phone: String, $cities_id: bigint, $file_number: bigint, $careers_id: bigint, $shared: Boolean, $username: String, $bio: String, $created_with_sn: Boolean, $user_status: Boolean, $user_categories_id: bigint, $avatar: String) 
+    {
+        insert_sh_persons (
+            objects: [
+                {
+                    gender: $gender,
+                    birth_date: $birth_date,
+                    phone: $phone,
+                    students: {
+                        data: [
+                            {
+                                cities_id: $cities_id,
+                                file_number: $file_number,
+                                careers_id: $careers_id,
+                                shared: $shared
+                            }
+                        ]
+                    },
+                    users: {
+                        data: [
+                            {
+                                username: $username,
+                                bio: $bio,
+                                created_with_sn: $created_with_sn, 
+                                user_status: $user_status,
+                                user_categories_id: $user_categories_id,
+                                avatar: $avatar
+                            }
+                        ]
+                    }
+                }
+            ]
+        ) {
+            returning {
+                id
+                firstname
+                lastname
+              	gender
+              	birth_date
+                users {
+                    id
+                    username
+                    email
+                  	bio
+                    created_with_sn
+                    user_category {
+                        id
+                        description
+                    }
+                }
+                students {
+                    id
+                    city {
+                        id
+                        name
+                      	state_id
+                    }
+                    career {
+                        id
+                        name
+                    }
+                }
+            }
+        }
     }
 `;
